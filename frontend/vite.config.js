eac7fa55b9f05dfaaabaebe0b347bec482fcbd2e
mesukabiru <mesukabiru@gmail.com>
2026-02-22 11:50:38 +0700

    update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,17 +1,19 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import path from 'path' // <-- Tambahkan ini
-import { fileURLToPath } from 'url' // <-- Tambahkan ini
+import path from 'path'
+import { fileURLToPath } from 'url'
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'), // <-- Tambahkan alias ini
+      '@': path.resolve(__dirname, './src'),
     },
   },
+  build: {
+    emptyOutDir: false
+  }
 })
